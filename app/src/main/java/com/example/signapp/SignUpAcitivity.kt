a9cf7a5bbe59e90e4f3363619722b16fa7695853
leonskyd <leonsky@inbox.ru>
2022-04-05 17:41:54 +0300

    1 этап. Сделать два экрана. Один основной, второй для регистрации

---
+++ app/src/main/java/com/example/signapp/SignUpAcitivity.kt
@@ -0,0 +1,11 @@
+package com.example.signapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
