47030a5d6ef43a5da62b9f16279eee72325ec419
Denis Leonskii <72574009+leonskyd@users.noreply.github.com>
2022-07-03 18:16:29 +0300

    Убрали проверку данных из Презентера

--- app/src/main/java/com/example/signapp/loginScreen/LoginPresenter.kt
+++ app/src/main/java/com/example/signapp/loginScreen/LoginPresenter.kt
@@ -1,12 +1,9 @@
 package com.example.signapp.loginScreen
 
-import android.os.SystemClock.sleep
-import com.example.signapp.model.Repository
-
 class LoginPresenter {
 
-    private val repository = Repository()
-    private var view: LoginView? = null
+    val loginInteractor = LoginInteractor()
+    var view: LoginView? = null
 
 
     fun onAttach(view: LoginView) {
@@ -14,44 +11,18 @@ class LoginPresenter {
     }
 
     fun onLogin(login: String, password: String) {
-        if(checkPassword(login, password)) {
+        if(loginInteractor.checkPassword(login, password)) {
             view?.setSuccess()
         } else {
             view?.setPasswordError() // выходит сообщение об ошибке
         }
     }
 
-    fun checkPassword(login: String, password: String): Boolean {
-        var passwordChecked = false
-        val userList = repository.loadUsers()
-        for (item in userList) {
-            if (item.userLogin.equals(login)
-                && item.userPassword.equals(password)
-            ) {
-                passwordChecked = true
-            }
-        }
-        return passwordChecked
-    }
-
-
-        fun checkLogin(login: String) {
-            if (!isLoginExist(login)) {
-                view?.setLoginError()
-            } else {
-                view?.setOneMoreLogin()
-            }
-        }
-
-        private fun isLoginExist(login: String): Boolean {
-            val userList = repository.loadUsers()
-            var found = false
-            for (item in userList) {
-                if (item.userLogin.equals(login)) {
-                    found = true
-                    break
-                }
-            }
-            return found
+    fun checkLogin(login: String) {
+        if (!loginInteractor.isLoginExist(login)) {
+            view?.setLoginError()
+        } else {
+            view?.setOneMoreLogin()
         }
     }
+}
